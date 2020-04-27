6f6e088087a50cb42c36dcc8c9297044a53b783a
William Kwon <williamkwon97@gmail.com>
2020-04-27 16:28:05 -0500

    first-commit

---
+++ finalproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finalproject
+//
+//  Created by William Kwon on 4/27/20.
+//  Copyright © 2020 groupproject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
